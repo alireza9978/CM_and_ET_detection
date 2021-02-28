d1a61fc8a00123d8f5777a865236451c3197f51d
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-02-28 10:26:47 +0330

    update functions for working with data frame

--- spark_main/functions.py
+++ spark_main/functions.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import pandas as pd
 import pyspark.sql.functions as f
@@ -67,3 +69,129 @@ def filter_data_set(temp_sdf, from_date="BEGIN", to_date="END", temp_id="*", val
 
 
 generate_feature_UDF = pandas_udf(generate_feature, returnType=ArrayType(FloatType()))
+
+
+def add_validation_column(temp_sdf):
+    def validation(x):
+        res = []
+        for row in x:
+            v = True
+            if (len(row) != 24 or  # unusual size
+                    (row >= 0).sum() != 24 or  # number of valid elements = 24
+                    # sum(n >= 0 for n in row) != 24 or
+                    # equal or more than 3 zero elements
+                    np.count_nonzero(row == 0) >= 3 or
+                    sum(n < 0 for n in row) > 0):  # not have negative element
+                v = False
+            res.append(v)
+        return pd.Series(res)
+
+    validation_UDF = pandas_udf(validation, returnType=BooleanType())
+    temp = temp_sdf.withColumn("V", validation_UDF(col("power")))
+    return temp
+
+
+# add "N"ormal consumption ("N"onmalicious) column
+def add_normal_column(temp_sdf):
+    N = True
+    temp = temp_sdf.withColumn("N", f.lit(N))
+    return temp
+
+
+# generate unique id
+def generate_unique_id(temp_sdf):
+    temp = temp_sdf
+    temp = temp.withColumn("uid", f.concat(col("id"), f.lit("-"), col("#")).alias("uid"))
+    return temp
+
+
+# Generate malicious samples
+def h1(x):
+    MAX = 0.8
+    MIN = 0.1
+    alpha = random.uniform(MIN, MAX)
+    temp = np.array(x)
+    return (temp * alpha).tolist()
+
+
+def h2(x):
+    MIN_OFF = 4  # hour
+    DURATION = random.randint(MIN_OFF, 23)
+    START = random.randint(0, 23 - DURATION) if DURATION != 23 else 0
+    END = START + DURATION
+    temp = []
+    for i in range(len(x)):
+        if i < START or i >= END:
+            temp.append(x[i])
+        else:
+            temp.append(0.0)
+    return temp
+
+
+def h3(x):
+    MAX = 0.8
+    MIN = 0.1
+    temp = []
+    for i in range(len(x)):
+        temp.append(x[i] * random.uniform(MIN, MAX))
+    return temp
+
+
+def h4(x):
+    MAX = 0.8
+    MIN = 0.1
+    mean = np.mean(x)
+    temp = []
+    for i in range(len(x)):
+        temp.append(mean * random.uniform(MIN, MAX))
+    return temp
+
+
+def h5(x):
+    mean = np.mean(x)
+    temp = []
+    for i in range(len(x)):
+        temp.append(mean)
+    return temp
+
+
+def h6(x):
+    temp = np.array(x)
+    # temp=temp[::-1]
+    temp = np.flipud(temp)
+    return temp.tolist()
+
+
+# add malicious samples
+def create_malicious_df(temp_sdf):
+    def random_attack_assigner(x):
+        NUMBER_OF_MALICIOUS_GENERATOR = 6
+        res = []
+        for row in x:
+            rand = random.randint(1, NUMBER_OF_MALICIOUS_GENERATOR)
+            if rand == 1:
+                temp = (h1(row))
+            elif rand == 2:
+                temp = (h2(row))
+            elif rand == 3:
+                temp = (h3(row))
+            elif rand == 4:
+                temp = (h4(row))
+            elif rand == 5:
+                temp = (h5(row))
+            elif rand == 6:
+                temp = (h6(row))
+            else:
+                temp = None
+            res.append(temp)
+        return pd.Series(res)
+
+    cols = temp_sdf.columns
+    random_attack_assigner_UDF = pandas_udf(random_attack_assigner, returnType=ArrayType(FloatType()))
+    N = False
+    temp_sdf = temp_sdf.withColumn("N", f.lit(N))  # malicious sample
+    # change '#' column number to negative
+    temp_sdf = temp_sdf.withColumn("#", col("#") * -1)
+    temp_sdf = temp_sdf.withColumn("power", random_attack_assigner_UDF(col("power")))
+    temp_sdf = add_statistics_column(temp_sdf)  # for update statistics
+    return temp_sdf.select(cols)  # to reorder columns
