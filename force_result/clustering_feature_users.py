0e0dcd974cca8d65db3daf188e0de3c3234e6d9b
alireza9978 <Ssspppnnn123>
2021-03-22 15:07:03 +0430

    split train and test on clustering

--- force_result/clustering_feature_users.py
+++ force_result/clustering_feature_users.py
@@ -15,15 +15,25 @@ def vector(temp_df: pd.DataFrame):
 
 labels = pd.read_csv("my_data/labels.csv")
 good_users = np.array(labels[labels.unknown != 1].img.tolist())
-df = load_data_frame()
-df = df.groupby("id").apply(vector)
-df = df.fillna(0)
-df = df.reset_index()
-users_id = df.id
-df = df.drop(columns=["id"])
+train_df, test_df = get_train_test()
 
-x = df.to_numpy()
-x = StandardScaler().fit_transform(x)
+train_df = train_df.groupby("id").apply(vector)
+train_df = train_df.fillna(0)
+train_df = train_df.reset_index()
+train_users_id = train_df.id
+train_df = train_df.drop(columns=["id"])
+
+test_df = test_df.groupby("id").apply(vector)
+test_df = test_df.fillna(0)
+test_df = test_df.reset_index()
+test_users_id = test_df.id
+test_df = test_df.drop(columns=["id"])
+
+x_train = train_df.to_numpy()
+x_test = test_df.to_numpy()
+clf = StandardScaler()
+x_train = clf.fit_transform(x_train)
+x_test = clf.transform(x_test)
 
 models = [
     KMeansConstrained(n_clusters=2, size_min=10, n_jobs=-1),
@@ -36,11 +46,11 @@ best = None
 best_value = 0
 silhouette_scores = []
 for i, model in enumerate(models):
-    y_prediction = model.fit_predict(x)
+    y_prediction = model.fit_predict(x_train)
 
     y_set = set(y_prediction)
     if len(y_set) > 1:
-        sil = metrics.silhouette_score(x, y_prediction)
+        sil = metrics.silhouette_score(x_train, y_prediction)
         silhouette_scores.append(sil)
         if best is None or sil > best_value:
             best = model
@@ -48,17 +58,26 @@ for i, model in enumerate(models):
     else:
         print("one cluster")
 
-y_prediction = best.predict(x)
-y_set = set(y_prediction)
+y_train_prediction = best.predict(x_train)
+y_test_prediction = best.predict(x_test)
+y_set = set(y_train_prediction)
 df = load_data_frame()
 df = df[df.id.isin(good_users)]
-users_clusters = []
+train_users_clusters = []
+test_users_clusters = []
 for cluster in y_set:
-    ss = users_id[y_prediction == cluster]
-    print(len(ss))
-    users_clusters.append(ss)
-    print("cluster {} count = {}".format(cluster, (y_prediction == cluster).sum()))
+    train_user_cluster_id = train_users_id[y_train_prediction == cluster]
+    test_user_cluster_id = train_users_id[y_train_prediction == cluster]
+    print(f"train_user_cluster_id {len(train_user_cluster_id)}")
+    print(f"test_user_cluster_id {len(test_user_cluster_id)}")
+    train_users_clusters.append(train_user_cluster_id)
+    test_users_clusters.append(test_user_cluster_id)
+    print("train_cluster {} count = {}".format(cluster, (y_train_prediction == cluster).sum()))
+    print("test_cluster {} count = {}".format(cluster, (y_test_prediction == cluster).sum()))
 print(best_value)
 
-with open('user_clusters.pkl', 'wb') as f:
-    pickle.dump(users_clusters, f)
+with open('train_user_clusters.pkl', 'wb') as f:
+    pickle.dump(train_users_clusters, f)
+
+with open('test_user_clusters.pkl', 'wb') as f:
+    pickle.dump(test_users_clusters, f)
