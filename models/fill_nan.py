83f355f18e3f21db5bd09dbece1bd6b40aa2d6d3
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-05-19 12:25:12 +0430

    add linear auto fill nan method

--- models/fill_nan.py
+++ models/fill_nan.py
@@ -8,6 +8,7 @@ class FillNanMode(Enum):
     drop = auto()
     from_previous_data = auto()
     from_next_data = auto()
+    linear_auto_fill = auto()
 
     def get_method(self):
         if self == FillNanMode.without:
@@ -18,13 +19,24 @@ class FillNanMode(Enum):
             return from_next_data_method
         if self == FillNanMode.from_previous_data:
             return from_previous_data_method
+        if self == FillNanMode.linear_auto_fill:
+            return linear_auto_fill
 
 
-# todo implement methods
+def _single_user_linear(temp_df: pd.DataFrame):
+    temp_df.usage = temp_df.usage.interpolate(method='linear', limit_direction='forward', axis=0).ffill()
+    return temp_df
+
+
+def linear_auto_fill(temp_df: pd.DataFrame):
+    return temp_df.groupby("id").apply(_single_user_linear)
+
+
 def drop_nan_data_method(temp_df: pd.DataFrame):
     return temp_df.dropna()
 
 
+# todo implement methods
 def from_next_data_method(temp_df: pd.DataFrame):
     pass
 
