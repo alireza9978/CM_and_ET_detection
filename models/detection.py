f1346f871aa17838e14797da617d05b4d850e57f
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-05-24 15:50:39 +0430

    detection method changes

--- models/detection.py
+++ models/detection.py
@@ -4,86 +4,83 @@ import pandas as pd
 from joblib import Parallel, delayed
 
 
-def _calculate_bands(temp_df: pd.DataFrame):
-    temp_df = temp_df.set_index("date")
-    my_temp_df = temp_df.resample(resample_value).agg({"usage": "sum"})
-    usage = my_temp_df.usage
-    my_temp_df["id"] = temp_df.id[0]
-    my_temp_df["mining"] = False
-    my_temp_df["theft"] = False
-    my_temp_df["to_use"] = True
-    start = moving_avg_windows_size
-    while start < usage.shape[0]:
-        temp_data = usage[start]
-        temp_window = usage.iloc[start - moving_avg_windows_size:][
-                          my_temp_df.iloc[start - moving_avg_windows_size:]["to_use"]][0:moving_avg_windows_size]
-
-        temp_avg = temp_window.mean()
-        temp_std = temp_window.std()
-        temp_upper = temp_avg + (temp_std * std_coe)
-        temp_lower = temp_avg - (temp_std * std_coe)
-
-        if temp_data > temp_upper:
-            step = 1
-            while True:
-                m_window = usage[start + step:start + step + m]
-                m_window_detect = (m_window > temp_upper) & (m_window > 20)
-                m_window_ok = m_window_detect.sum() < n
-                if m_window_ok:
-                    break
-                step += 1
-            if step > 1:
-                my_temp_df.iloc[start + 1:start + step + m, 2] = (usage[start + 1:start + step + m] > temp_upper)
-                my_temp_df.iloc[start + 1:start + step + m, 4] = (usage[start + 1:start + step + m] < temp_upper)
-                start = start + moving_avg_windows_size + step
-
-        if temp_data < temp_lower:
-            step = 1
-            while True:
-                m_window = usage[start + step:start + step + m]
-                m_window_detect = (m_window < temp_lower)
-                m_window_ok = m_window_detect.sum() < n
-                if m_window_ok:
-                    break
-                step += 1
-            if step > 1:
-                my_temp_df.iloc[start + 1:start + step + m, 3] = (usage[start + 1:start + step + m] < temp_lower)
-                my_temp_df.iloc[start + 1:start + step + m, 4] = (usage[start + 1:start + step + m] > temp_lower)
-                start = start + moving_avg_windows_size + step
-
-        start += 1
-    return my_temp_df
+class DetectionParam:
+    def __init__(self, resample_value: str = "1D", moving_avg_windows_size: int = 40, std: float = 2.5, m: int = 14,
+                 n: int = 10):
+        self.resample_value = resample_value
+        self.moving_avg_windows_size = moving_avg_windows_size
+        self.std_coe = std
+        self.m = m
+        self.n = n
 
 
-def _apply_parallel(data_frame_grouped, func):
-    result_list = Parallel(n_jobs=multiprocessing.cpu_count())(
-        delayed(func)(group) for name, group in data_frame_grouped)
-    return pd.concat(result_list)
+class Detection:
+    def __init__(self, resample: str, windows_size: int, std: float, new_m: int, new_n: int):
+        self.params = DetectionParam(resample, windows_size, std, new_m, new_n)
 
+    def _calculate_bands(self, temp_df: pd.DataFrame):
+        temp_df = temp_df.set_index("date")
+        my_temp_df = temp_df.resample(self.params.resample_value).agg({"usage": "sum"})
+        usage = my_temp_df.usage
+        my_temp_df["id"] = temp_df.id[0]
+        my_temp_df["mining"] = False
+        my_temp_df["theft"] = False
+        my_temp_df["to_use"] = True
+        start = self.params.moving_avg_windows_size
+        while start < usage.shape[0]:
+            temp_data = usage[start]
+            temp_window = usage.iloc[start - self.params.moving_avg_windows_size:][
+                              my_temp_df.iloc[start - self.params.moving_avg_windows_size:]["to_use"]][
+                          0:self.params.moving_avg_windows_size]
 
-def detect(temp_df: pd.DataFrame):
-    temp_df = _apply_parallel(temp_df.groupby(["id"]), _calculate_bands)
-    x = temp_df[["mining", "theft", "id"]].reset_index().groupby(["id"]).sum()
-    suspect = x[(x.mining > 0) | (x.theft > 0)].reset_index()['id'].unique()
-    temp_df = temp_df[temp_df.id.isin(suspect)]
-    return temp_df
+            temp_avg = temp_window.mean()
+            temp_std = temp_window.std()
+            temp_upper = temp_avg + (temp_std * self.params.std_coe)
+            temp_lower = temp_avg - (temp_std * self.params.std_coe)
 
+            if temp_data > temp_upper:
+                step = 1
+                while True:
+                    m_window = usage[start + step:start + step + self.params.m]
+                    m_window_detect = (m_window > temp_upper) & (m_window > 20)
+                    m_window_ok = m_window_detect.sum() < self.params.n
+                    if m_window_ok:
+                        break
+                    step += 1
+                if step > 1:
+                    my_temp_df.iloc[start + 1:start + step + self.params.m, 2] = (
+                            usage[start + 1:start + step + self.params.m] > temp_upper)
+                    my_temp_df.iloc[start + 1:start + step + self.params.m, 4] = (
+                            usage[start + 1:start + step + self.params.m] < temp_upper)
+                    start = start + self.params.moving_avg_windows_size + step
 
-def set_detection_parameters(resample: str, windows_size: int, std: float, new_m: int, new_n: int):
-    global resample_value
-    global moving_avg_windows_size
-    global std_coe
-    global m
-    global n
-    resample_value = resample
-    moving_avg_windows_size = windows_size
-    std_coe = std
-    m = new_m
-    n = new_n
+            if temp_data < temp_lower:
+                step = 1
+                while True:
+                    m_window = usage[start + step:start + step + self.params.m]
+                    m_window_detect = (m_window < temp_lower)
+                    m_window_ok = m_window_detect.sum() < self.params.n
+                    if m_window_ok:
+                        break
+                    step += 1
+                if step > 1:
+                    my_temp_df.iloc[start + 1:start + step + self.params.m, 3] = (
+                            usage[start + 1:start + step + self.params.m] < temp_lower)
+                    my_temp_df.iloc[start + 1:start + step + self.params.m, 4] = (
+                            usage[start + 1:start + step + self.params.m] > temp_lower)
+                    start = start + self.params.moving_avg_windows_size + step
 
+            start += 1
+        return my_temp_df
 
-resample_value = "{}H".format(24)
-moving_avg_windows_size = 40
-std_coe = 2.5
-m = 20
-n = 15
+    def _apply_parallel(self, data_frame_grouped, func):
+        result_list = Parallel(n_jobs=multiprocessing.cpu_count())(
+            delayed(func)(group) for name, group in data_frame_grouped)
+        return pd.concat(result_list)
+
+    def detect(self, temp_df: pd.DataFrame):
+        temp_df = self._apply_parallel(temp_df.groupby(["id"]), self._calculate_bands)
+        x = temp_df[["mining", "theft", "id"]].reset_index().groupby(["id"]).sum()
+        suspect = x[(x.mining > 0) | (x.theft > 0)].reset_index()['id'].unique()
+        temp_df = temp_df[temp_df.id.isin(suspect)]
+        return temp_df
--- models/test.py
+++ models/test.py
@@ -1,18 +1,21 @@
 from models.Preprocessing import load_data_frame
-from models.detection import detect, set_detection_parameters
+from models.detection import Detection
 from models.fill_nan import FillNanMode
 from models.visualization import plot_detection
 
 if __name__ == '__main__':
-    path = "../my_data/all_data.csv"
+    path = "../sample_data/hourly_sample_accumulative-usage_gregorian-date.csv"
     hourly_df = load_data_frame(path, False, True, FillNanMode.drop)
+    # hourly_df = select_one_user(hourly_df, user_id=132)
 
     # path = "../sample_data/monthly_sample_non-accumulative-usage_gregorian-date.csv"
     # monthly_df = load_data_frame(path, True, False, FillNanMode.without)
 
     # hourly_df = select_random_user(hourly_df)
-    set_detection_parameters("1D", 30, 2.5, 10, 8)
-    detection = detect(hourly_df)
+    detection_clf = Detection("7D", 30, 2.5, 14, 8)
+    detection = detection_clf.detect(hourly_df)
+    print(detection.shape)
+    print(len(detection.id.unique()))
     for user_id in detection.id.unique():
         plot_detection(detection, user_id, "../my_figures/mining/{}.jpeg".format(user_id), mining=True)
         plot_detection(detection, user_id, "../my_figures/theft/{}.jpeg".format(user_id), theft=True)
