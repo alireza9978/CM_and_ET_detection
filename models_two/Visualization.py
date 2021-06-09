49e38b6cc4b7d4038d7d1a9b57a5abaf84240233
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-06-09 10:38:35 +0430

    add anomaly randomly to user

--- models_two/Visualization.py
+++ models_two/Visualization.py
@@ -10,7 +10,19 @@ def plot(temp_df: pd.DataFrame):
         plt.plot(inner_temp_df.index, inner_temp_df.usage, label="usage")
         plt.tight_layout()
         plt.legend()
-        plt.savefig("my_figures/irish/{}_{}.jpeg".format(user_id, resample_value))
+        plt.savefig("my_figures/irish/{}_{}_{}.jpeg".format(user_id, resample_value, real))
+        plt.close()
+
+
+def plot_anomaly(temp_df: pd.DataFrame, real="real"):
+    user_id = temp_df['id'].values[0]
+    resample_values = ["1H", "1D", "7D"]
+    for resample_value in resample_values:
+        inner_temp_df = temp_df[['usage']].resample(resample_value).sum()
+        plt.plot(inner_temp_df.index, inner_temp_df.usage, label="usage")
+        plt.tight_layout()
+        plt.legend()
+        plt.savefig("my_figures/irish/{}_{}_{}.jpeg".format(user_id, resample_value, real))
         plt.close()
 
 
@@ -27,8 +39,8 @@ def plot_mean_usage(temp_df: pd.DataFrame):
 
 if __name__ == '__main__':
     path = "/mnt/79e06c5d-876b-45fd-a066-c9aac1a1c932/Dataset/Power Distribution/irish.csv"
-    # df = next(pd.read_csv(path, chunksize=100000, converters={"date": pd.to_datetime}))
-    df = pd.read_csv(path, chunksize=100000, converters={"date": pd.to_datetime})
+    df = next(pd.read_csv(path, chunksize=100000, converters={"date": pd.to_datetime}))
+    # df = pd.read_csv(path, converters={"date": pd.to_datetime})
 
     df.set_index("date").groupby("id").apply(plot)
     df.set_index("date").groupby("id").apply(plot_mean_usage)
