ec137387b9bdb6d08dd59d09b7d61ba982fc191b
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-06-09 11:57:40 +0430

    add anomaly randomly to all user then save and plot

--- models_two/AddAnomaly.py
+++ models_two/AddAnomaly.py
@@ -11,7 +11,7 @@ def reduce_usage_randomly(temp_df: pd.DataFrame):
     date_range = end - start
     anomaly_start = start + (date_range / 4)
 
-    # 0.2 ~ 0.8
+    # 0.2 ~ 0.5
     reduction_coe = (random.random() * 0.3) + 0.2
     temp_start = anomaly_start + ((end - anomaly_start) * random.random() * 0.5)
     temp_end = temp_start + ((end - anomaly_start) * random.random())
@@ -20,14 +20,15 @@ def reduce_usage_randomly(temp_df: pd.DataFrame):
     temp_end = min(temp_end, end)
 
     temp_df.loc[temp_start:temp_end, 'usage'] = temp_df.loc[temp_start:temp_end, 'usage'] * reduction_coe
+    temp_df["anomaly"] = False
+    temp_df[temp_start:temp_end, "anomaly"] = True
 
-    print("id", temp_df['id'].values[0], " start", temp_start, " end", temp_end, "reduction_coe", reduction_coe)
     return temp_df
 
 
 if __name__ == '__main__':
     path = "/mnt/79e06c5d-876b-45fd-a066-c9aac1a1c932/Dataset/Power Distribution/irish.csv"
-    df = next(pd.read_csv(path, chunksize=100000, converters={"date": pd.to_datetime}))
+    df = pd.read_csv(path, converters={"date": pd.to_datetime})
     # df = pd.read_csv(path, chunksize=100000, converters={"date": pd.to_datetime})
 
     anomaly_df = df.set_index("date").groupby("id").apply(reduce_usage_randomly)
@@ -36,3 +37,5 @@ if __name__ == '__main__':
         plot_anomaly(user_df, "anomaly")
         user_df = df[df.id == user].set_index("date")
         plot_anomaly(user_df, "clean")
+
+    anomaly_df.to_csv("my_data/irish_anomaly.csv", index=None)
