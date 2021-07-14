0d4bcf17bc284335282b031109f3ad02d0d2a227
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-07-14 18:08:54 +0430

    plot one user data and it's matrix profile add anomaly and plot these again

--- mashhad_test/Main.py
+++ mashhad_test/Main.py
@@ -1,18 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import stumpy
+
+from mashhad_test.CreateGrayScaleImage import make_data_set_single_user
 from models.Preprocessing import load_data_frame
 from models.fill_nan import FillNanMode
-from models.filters import *
-from models.detection import *
-from mashhad_test.Visualization import plot
-from models.visualization import plot_detection
+from models.filters import data_frame_agg
+from models_two.AddAnomaly import reduce_usage_randomly
 
 if __name__ == '__main__':
-    temp_df = load_data_frame("my_data/mashhad_withNan.csv", False, False, FillNanMode.linear_auto_fill)
-    temp_df = usage_mean_above_input_percent(temp_df, 0.8, "1D", 50)
-    temp_df = usage_mean_above_input_percent(temp_df, 0.8, "7D", 400)
-    temp_df.set_index("date").groupby("id").apply(plot)
-    model = Detection("7D", 20, 2.5, 10, 8)
-    detection = model.detect(temp_df)
-    print(detection.shape)
-    print(len(detection.id.unique()))
-    for user_id in detection.id.unique():
-        plot_detection(detection, user_id, "my_figures/mashhad_mining/{}.jpeg".format(user_id), mining=True)
+    temp_df = load_data_frame("/mnt/79e06c5d-876b-45fd-a066-c9aac1a1c932/Dataset/Power Distribution/irish.csv", False,
+                              False, FillNanMode.without, True)
+    temp_users_id = np.random.choice(temp_df.id, 1)
+    # temp_users_id = np.array([5010892])
+    temp_df = temp_df[temp_df.id == temp_users_id[0]]
+    make_data_set_single_user(temp_df)
+
+    temp_df_agg = data_frame_agg(temp_df, "7D")
+
+    x = temp_df_agg[["usage", "date"]]
+    x = x.reset_index()
+    ts = x.usage.to_numpy().reshape(-1)
+    window_size = 4  # Approximately, how many data points might be found in a pattern
+
+    matrix_profile = stumpy.stump(ts, m=window_size)
+
+    plt.plot(ts)
+    plt.savefig("my_figures/matrix_profiles/user_data.jpeg")
+    plt.close()
+    plt.plot(matrix_profile[:, 0])
+    plt.savefig("my_figures/matrix_profiles/user_profile.jpeg")
+    plt.close()
+
+    anomaly_df = temp_df.set_index("date").groupby("id").apply(reduce_usage_randomly)
+    anomaly_df = anomaly_df.reset_index()
+    anomaly_df = data_frame_agg(anomaly_df, "7D")
+
+    x = anomaly_df[["usage", "date"]]
+    x = x.reset_index()
+    ts = x.usage.to_numpy().reshape(-1)
+    window_size = 4  # Approximately, how many data points might be found in a pattern
+
+    matrix_profile = stumpy.stump(ts, m=window_size)
+
+    plt.plot(ts)
+    plt.savefig("my_figures/matrix_profiles/user_data_anomalous.jpeg")
+    plt.close()
+    plt.plot(matrix_profile[:, 0])
+    plt.savefig("my_figures/matrix_profiles/user_profile_anomalous.jpeg")
+    plt.close()
