a901c53f270f403a14b67ce5a05618d3970db72e
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-07-30 12:27:39 +0430

    agglomerative clustering with dtw as distance metric

---
+++ models_two/ClusterUser.py
@@ -0,0 +1,86 @@
+from multiprocessing import cpu_count
+
+import numpy as np
+from joblib import Parallel, delayed
+from tslearn.metrics import dtw
+from tslearn.utils import to_time_series_dataset
+
+from models.Preprocessing import load_data_frame
+from models.fill_nan import FillNanMode
+from models.filters import data_frame_agg
+from models_two.LinkedList import Node, SLinkedList
+
+path = "/mnt/79e06c5d-876b-45fd-a066-c9aac1a1c932/Dataset/Power Distribution/irish.csv"
+df = load_data_frame(path, False, False, FillNanMode.linear_auto_fill, True)
+df = data_frame_agg(df, "1D")
+df = to_time_series_dataset(df.groupby("id").apply(lambda x: x.usage.to_numpy()).to_numpy())
+df = df.reshape(df.shape[0], df.shape[1])
+print(df.shape)
+
+
+def process(i, j):
+    return i, j, dtw(df[i], df[j])
+
+
+results = Parallel(n_jobs=cpu_count())(
+    delayed(process)(i, j) for i in range(df.shape[0]) for j in range(i + 1, df.shape[0]))
+
+distance_matrix = np.zeros((df.shape[0], df.shape[0]))
+for row in results:
+    distance_matrix[row[0]][row[1]] = row[2]
+    distance_matrix[row[1]][row[0]] = row[2]
+distance_matrix[distance_matrix == 0] = np.inf
+
+
+def find_minimum_distance(matrix):
+    index = np.unravel_index(np.argmin(matrix, axis=None), matrix.shape)
+    return index
+    # value = distance_matrix[index[0]][index[1]]
+    # pass
+
+
+def merge_two_cluster(index):
+    minimum = min(index[0], index[1])
+    maximum = max(index[0], index[1])
+    clusters_list[minimum].add_all(clusters_list[maximum])
+    clusters_list[maximum] = None
+
+
+def update_distance_matrix(clusters):
+    count = len(clusters)
+    matrix = np.zeros((count, count))
+    matrix[matrix == 0] = np.inf
+    for i in range(len(clusters)):
+        if clusters[i]:
+            cluster_one_items = clusters[i].list()
+            for j in range(i + 1, len(clusters)):
+                if clusters[j]:
+                    cluster_two_items = clusters[j].list()
+                    total_distance = 0
+                    for item in cluster_one_items:
+                        for other_item in cluster_two_items:
+                            total_distance += distance_matrix[item][other_item]
+                    total_distance = total_distance / (len(cluster_two_items) * len(cluster_one_items))
+                    matrix[i][j] = total_distance
+                    matrix[j][i] = total_distance
+    return matrix
+
+
+def print_clusters(clusters):
+    for cluster in clusters:
+        if cluster:
+            cluster.list_print()
+            print("")
+
+    print()
+
+
+clusters_list = [SLinkedList(Node(i)) for i in range(df.shape[0])]
+clusters_count = df.shape[0]
+
+for iteration in range(df.shape[0]):
+    temp_distance_matrix = update_distance_matrix(clusters_list)
+    nearest_cluster = find_minimum_distance(temp_distance_matrix)
+    merge_two_cluster(nearest_cluster)
+    print_clusters(clusters_list)
+    clusters_count -= 1
---
+++ models_two/LinkedList.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, data_value=None):
+        self.data_value = data_value
+        self.next_value = None
+
+
+class SLinkedList:
+    def __init__(self, head_node=None):
+        self.head_value = head_node
+
+    def at_beginning(self, new_data):
+        new_node = Node(new_data)
+        new_node.next_value = self.head_value
+        self.head_value = new_node
+
+    def in_betwen(self, middle_node, new_data):
+        if middle_node is None:
+            print("The mentioned node is absent")
+            return
+        new_node = Node(new_data)
+        new_node.next_value = middle_node.nextval
+        middle_node.next_value = new_node
+
+    def at_end(self, new_data):
+        new_node = Node(new_data)
+        if self.head_value is None:
+            self.head_value = new_node
+            return
+        temp_node = self.head_value
+        while temp_node.next_value:
+            temp_node = temp_node.nextval
+        temp_node.next_value = new_node
+
+    def add_all(self, new_list):
+        temp_value = new_list.head_value
+        while temp_value is not None:
+            temp_temp_value = temp_value.next_value
+            temp_value.next_value = self.head_value
+            self.head_value = temp_value
+            temp_value = temp_temp_value
+
+    # Print the linked list
+    def list_print(self):
+        print_value = self.head_value
+        while print_value is not None:
+            print(print_value.data_value, end=", ")
+            print_value = print_value.next_value
+
+    def list(self):
+        out = []
+        temp_value = self.head_value
+        while temp_value is not None:
+            out.append(temp_value.data_value)
+            temp_value = temp_value.next_value
+        return out
