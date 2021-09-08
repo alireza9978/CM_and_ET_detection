b02c9d5f8d6587d1d3353c72adae2a0f60be3860
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-09-08 21:31:53 +0430

    try to find pattern

--- models_two/PatternExtraction/One.py
+++ models_two/PatternExtraction/One.py
@@ -1,10 +1,11 @@
-from random import random
 from multiprocessing import cpu_count
-from joblib import Parallel, delayed
+from multiprocessing import cpu_count
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sklearn.utils
+from joblib import Parallel, delayed
 from scipy.cluster.hierarchy import dendrogram
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.preprocessing import MinMaxScaler
@@ -14,24 +15,6 @@ from models.Preprocessing import load_data_frame
 from models.fill_nan import FillNanMode
 from models.filters import data_frame_agg
 from models_two.AddAttacksToUser import attack1, attack2, attack3, attack4, attack5, attack6
-
-states = [["1H", 24]]
-
-# load data set
-path = "/mnt/79e06c5d-876b-45fd-a066-c9aac1a1c932/Dataset/Power Distribution/irish.csv"
-df = load_data_frame(path, False, False, FillNanMode.linear_auto_fill, True)
-
-# remove some part of data to make sure all of data start from same date
-maximum_start_date = df[["id", "date"]].groupby("id").min().date.unique().max()
-df = df[df.date >= pd.to_datetime(maximum_start_date)]
-
-# remove data of users that their data points count is less than average
-user_data_points_count = df[["id", "usage"]].groupby("id").count()
-proper_users_ids = (user_data_points_count[user_data_points_count >= user_data_points_count.mean()].dropna()).index
-df = df[df.id.isin(proper_users_ids)]
-
-# define attacks
-attacks = [attack1, attack2, attack3, attack4, attack5, attack6]
 
 
 def plot_dendrogram(model, **kwargs):
@@ -107,53 +90,74 @@ def calculate_distance_matrix(temp_df):
     return distance_matrix
 
 
-for state in states:
-    # convert user usage to same length vectors
-    temp_vec, labels = vectorization(df, state[0], state[1])
-    print(temp_vec.shape)
+if __name__ == '__main__':
 
-    # select a smaller part of vectors randomly to reduce the calculation
-    sampled_index = np.random.choice(np.arange(temp_vec.shape[0]), int(0.1 * temp_vec.shape[0]), replace=False)
-    # labels = labels[sampled_index]
-    temp_vec = temp_vec[sampled_index]
-    print(temp_vec.shape)
+    states = [["1H", 24]]
 
-    # scale all values to lie between zero and one
-    scl = MinMaxScaler()
-    temp_vec = scl.fit_transform(temp_vec)
+    # load data set
+    path = "/mnt/79e06c5d-876b-45fd-a066-c9aac1a1c932/Dataset/Power Distribution/irish.csv"
+    df = load_data_frame(path, False, False, FillNanMode.linear_auto_fill, True)
 
-    # cluster usage vectors to find unique clusters
-    # clu = DBSCAN(0.2, min_samples=50, metric=dtw)
-    # clu = DBSCAN(0.25, min_samples=10)
-    # answer = clu.predict(temp_vec)
-    # plt.hist(answer)
-    # plt.show()
+    # remove some part of data to make sure all of data start from same date
+    maximum_start_date = df[["id", "date"]].groupby("id").min().date.unique().max()
+    df = df[df.date >= pd.to_datetime(maximum_start_date)]
 
-    clu = AgglomerativeClustering(distance_threshold=0.75, n_clusters=None, affinity="precomputed", linkage="average")
-    matrix = calculate_distance_matrix(temp_vec)
-    clu = clu.fit(matrix)
-    plt.figure(1, (15, 8))
-    plt.title('Hierarchical Clustering Dendrogram')
-    # plot the top three levels of the dendrogram
-    plot_dendrogram(clu, truncate_mode='level', p=5)
-    plt.xlabel("Number of points in node (or index of point if no parenthesis).")
-    plt.show()
-    plt.close()
+    # remove data of users that their data points count is less than average
+    user_data_points_count = df[["id", "usage"]].groupby("id").count()
+    proper_users_ids = (user_data_points_count[user_data_points_count >= user_data_points_count.mean()].dropna()).index
+    df = df[df.id.isin(proper_users_ids)]
 
-    plt.hist(clu.labels_)
-    plt.show()
-    plt.close()
+    # define attacks
+    attacks = [attack1, attack2, attack3, attack4, attack5, attack6]
 
-    print(np.unique(clu.labels_, return_counts=True))
+    for state in states:
+        # convert user usage to same length vectors
+        temp_vec, labels = vectorization(df, state[0], state[1])
+        print(temp_vec.shape)
 
-    plt.figure(1, (10, 15))
-    centers = []
-    for i in range(clu.n_clusters_):
-        temp_center = np.mean(temp_vec[clu.labels_ == i], axis=0)
-        axe = plt.subplot(clu.n_clusters_, 1, i + 1)
-        axe.plot(temp_center)
-        centers.append(temp_center)
-    plt.show()
-    plt.close()
+        # select a smaller part of vectors randomly to reduce the calculation
+        sampled_index = np.random.choice(np.arange(temp_vec.shape[0]), int(0.1 * temp_vec.shape[0]), replace=False)
+        # labels = labels[sampled_index]
+        temp_vec = temp_vec[sampled_index]
+        print(temp_vec.shape)
 
-    print(clu)
+        # scale all values to lie between zero and one
+        scl = MinMaxScaler()
+        temp_vec = scl.fit_transform(temp_vec)
+
+        # cluster usage vectors to find unique clusters
+        # clu = DBSCAN(0.2, min_samples=50, metric=dtw)
+        # clu = DBSCAN(0.25, min_samples=10)
+        # answer = clu.predict(temp_vec)
+        # plt.hist(answer)
+        # plt.show()
+
+        clu = AgglomerativeClustering(distance_threshold=0.75, n_clusters=None, affinity="precomputed",
+                                      linkage="average")
+        matrix = calculate_distance_matrix(temp_vec)
+        clu = clu.fit(matrix)
+        plt.figure(1, (15, 8))
+        plt.title('Hierarchical Clustering Dendrogram')
+        # plot the top three levels of the dendrogram
+        plot_dendrogram(clu, truncate_mode='level', p=5)
+        plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+        plt.show()
+        plt.close()
+
+        plt.hist(clu.labels_)
+        plt.show()
+        plt.close()
+
+        print(np.unique(clu.labels_, return_counts=True))
+
+        plt.figure(1, (10, 15))
+        centers = []
+        for i in range(clu.n_clusters_):
+            temp_center = np.mean(temp_vec[clu.labels_ == i], axis=0)
+            axe = plt.subplot(clu.n_clusters_, 1, i + 1)
+            axe.plot(temp_center)
+            centers.append(temp_center)
+        plt.show()
+        plt.close()
+
+        print(clu)
--- models_two/PatternExtraction/Two.py
+++ models_two/PatternExtraction/Two.py
@@ -1,15 +1,90 @@
+import numpy as np
+import pandas
 import pandas as pd
+import sklearn
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv("../../my_data/MHEALTHDATASET/health.csv")
 
-segment_count = 4000
+segment_length = 100
+
 
 def vectorization(temp_df: pd.DataFrame):
-    size = temp_df.shape[0] // segment_count
+    temp_df = temp_df.reset_index(drop=True)
+    temp_vectors = np.ndarray(shape=(0, segment_length, 23))
+    labels_vector = np.ndarray(shape=(0, 1))
+    start = 0
+    end = start + 1
+    max_end = temp_df.shape[0]
+    while True:
+        last_label = temp_df.label[start]
+        while temp_df.label[end] == last_label:
+            end += 1
+            if end >= max_end:
+                break
 
-    temp_df["segment"] = temp_df["order"] // size
-    good_segment = temp_df[['id', 'segment']].groupby("segment").count() == size
-    temp_df = temp_df[temp_df.segment.isin(good_segment[good_segment["id"]].index)]
+        if last_label == 0:
+            start = end
+            end += 1
+            if end >= max_end:
+                break
+            continue
+        small_df = temp_df[start:end]
+
+        small_df["segment"] = small_df["order"] // segment_length
+        good_segment = small_df[['id', 'segment']].groupby("segment").count() == segment_length
+        small_df = small_df[small_df.segment.isin(good_segment[good_segment["id"]].index)]
+        temp_vec = small_df.drop(columns=["label", "segment", "id", "order"]).to_numpy().reshape(-1, segment_length, 23)
+        temp_vectors = np.concatenate([temp_vectors, temp_vec])
+        labels_vector = np.concatenate([labels_vector, np.full((temp_vec.shape[0], 1), last_label, np.int)])
+        start = end
+        end += 1
+        if end >= max_end:
+            break
+
+    return temp_vectors, labels_vector
 
 
+# to speed up process
+df = df[df.id == 1]
 vec = df.groupby("id").apply(vectorization)
+
+vectors = vec[1][0]
+labels = vec[1][1]
+vectors, labels = sklearn.utils.shuffle(vectors, labels)
+
+for i in range(vectors.shape[2]):
+    one_feature = vectors[:, :, i]
+
+    # train_x, test_x, train_y, test_y = train_test_split(one_feature, labels, test_size=0.33, random_state=42)
+
+    scaler = MinMaxScaler()
+    train_x = scaler.fit_transform(one_feature)
+    train_y = labels
+    # test_x = scaler.transform(test_x)
+
+    clu = AgglomerativeClustering(distance_threshold=8, n_clusters=None)
+    clu = clu.fit(train_x)
+
+    # plt.figure(1, (15, 8))
+    # plt.title('Hierarchical Clustering Dendrogram')
+    # # plot the top three levels of the dendrogram
+    # plot_dendrogram(clu, truncate_mode='level', p=5)
+    # plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+    # plt.show()
+    # plt.close()
+
+
+    def inner(inner_df: pd.DataFrame):
+        inner_df.columns = ["value", "label"]
+        return inner_df.groupby("label").count().transpose()
+
+    result_df = pd.DataFrame(np.stack([clu.labels_, train_y.squeeze()], axis=1), columns=["cluster", "label"])
+    result_df = result_df.groupby("cluster").apply(inner)
+    result_df = result_df.reset_index(level=1, drop=True)
+    result_df = result_df.apply(lambda row: row / row.sum(), axis=1)
+    result_df = result_df.apply(lambda row: pd.Series([row.max(), row.idxmax()]), axis=1)
+
+    print(result_df[result_df[0] > 0.75])
